95c318c24a93979bbbb34a9405e0c60cb63fecfb
Theimis <theimislimalopes422@gmail.com>
2024-06-05 14:54:35 -0300

    remove empty line

--- src/services/photos.js
+++ src/services/photos.js
@@ -3,7 +3,6 @@ export const getPhotos = async (photosPerPage, callback) => {
   const baseUrl = "https://api.pexels.com/v1/curated";
 
   let response = [];
-
   try {
     response = await fetch(`${baseUrl}?page=1&per_page=${photosPerPage}`, {
       method: "get",
